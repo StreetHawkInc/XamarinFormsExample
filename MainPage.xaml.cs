c437089cf8135d73314660ae41458208b3ee01b6
StreethawkNick <nick@streethawk.com>
2017-05-25 00:24:20 +1000

    Add files via upload
    Changed home screen

---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Com.Streethawk.Library.Core;
+using Com.Streethawk.Library.Push;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinExample
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
